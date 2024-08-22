1cef2c7e1e104ae6675fc99d79c9460730722367
YuanGao <140305042+YYYuanGao@users.noreply.github.com>
2024-08-22 17:16:37 +0800

    Update T1_prop.sh

--- T1_prop.sh
+++ T1_prop.sh
@@ -1,4 +1,4 @@
-SUBJECTS_DIR=/home/student2/2022_WMPFC/JiaKe_20230928_JiaK_WMPFC_sub010_2/T1
+SUBJECTS_DIR=/home/student2/2022_WMPFC/WMPFC_sub010_2/T1
 
 
 recon-all -all -s OUTPUT.nii -hires -i s.nii -expert expert.opt
